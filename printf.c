67498ef2ded55961bcd9129ce5aff995e5313393
oumaima <oumaimanaanaapro@gmail.com>
2023-09-17 14:09:54 +0100

    checking betty with switch case

--- printf.c
+++ printf.c
@@ -9,35 +9,37 @@
 
 int _printf(const char *format, ...)
 {
-	int i, j, num = 0, len = 0;
+	int i, num = 0, len = 0;
 	va_list type;
 
-	data spec[] = {
-		{'s', _str},
-		{'c', _char},
-		{'%', _mod},
-	};
-
 	va_start(type, format);
-
 	if (format == NULL)
 		return (-1);
-
 	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			if (format[i + 1] != '\0')
 			{
-				for (j = 0; j < 3; j++)
+				switch (format[i + 1])
 				{
-					if (format[i + 1] == spec[j].s)
-					{
-						len = spec[j].f(type);
+					case 's':
+						len = _str(type);
 						i++;
 						num += len;
 						break;
-					}
+					case 'c':
+						len = _char(type);
+						i++;
+						num += len;
+						break;
+					case '%':
+						len = _mod(type);
+						i++;
+						num += len;
+						break;
+					default:
+						break;
 				}
 			}
 		}
@@ -47,31 +49,6 @@ int _printf(const char *format, ...)
 			num++;
 		}
 	}
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] != '\0')
-			{
-				for (j = 0; j < 3; j++)
-				{
-					if (format[i + 1] == spec[j].s)
-					{
-						len = spec[j].f(type);
-						i++;
-						num += len;
-						break;
-					}
-				}
-			}
-		}
-		else
-		{
-			write(1, &format[i], 1);
-			num++;
-		}
-	}
-
 	va_end(type);
 	return (num);
 }
