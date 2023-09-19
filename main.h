7841a688ed5b613de2a532aabbb0bc97cccc9e58
Matsadura <zidanditoo1@gmail.com>
2023-09-19 21:21:34 +0100

    Remove structure from prievious version

--- main.h
+++ main.h
@@ -6,18 +6,6 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-/**
- * struct format - The format
- * @s: the specifier
- * @f: pointer to the specifier's function
- */
-
-typedef struct format
-{
-	char s;
-	int (*f)(va_list type);
-} data;
-
 
 int _char(va_list type);
 int _str(va_list type);
